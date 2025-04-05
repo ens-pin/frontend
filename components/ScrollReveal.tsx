bcefb5704412e5af3c7e4e2d1ea41eddc7c9fff4
AaronTan11 <tanweihup@gmail.com>
2025-04-06 07:27:33 +0800

    Disable markers in ScrollReveal component for improved performance.

--- components/ScrollReveal.tsx
+++ components/ScrollReveal.tsx
@@ -69,7 +69,7 @@ const ScrollReveal: React.FC<ScrollRevealProps> = ({
           start: "top bottom",
           end: rotationEnd,
           scrub: true,
-          markers: true,
+          markers: false,
         },
       }
     );
